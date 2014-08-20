7571042d8a1352d8fc24d3399869baa36cef9a7a
tinyjingle1 <tinyjingle@gmail.com>
2014-08-20 18:05:33 -0400

    More stuffs

--- public/scripts/game-view.js
+++ public/scripts/game-view.js
@@ -1,10 +1,19 @@
 (function() {
 
     var GameView = function() {
+        this.gameStarted = false;
     };
 
     GameView.prototype.render = function() {
-        // TODO
+        var gameContainer = $('#game');
+        var randomNum = Math.floor(Math.random() * 122);
+
+        gameContainer.html('');
+
+
+        for (var i = 0; i < 121; i++) {
+            gameContainer
+        }
     };
 
     window.GameView = GameView;
