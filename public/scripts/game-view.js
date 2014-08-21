e1b8ac0d4c1ed23f26c0fed95a5302932063829a
tinyjingle1 <tinyjingle@gmail.com>
2014-08-21 17:54:35 -0400

    Blocks

--- public/scripts/game-view.js
+++ public/scripts/game-view.js
@@ -6,13 +6,17 @@
 
     GameView.prototype.render = function() {
         var gameContainer = $('#game');
-        var randomNum = Math.floor(Math.random() * 122);
+        var randomNum = Math.floor(Math.random() * 121);
 
         gameContainer.html('');
 
-
+        var square = '<div class="block"></div>';
         for (var i = 0; i < 121; i++) {
-            gameContainer
+            if (i === randomNum) {
+                gameContainer.append('<div class="block" style="opacity: 0.95"></div>');
+            } else {
+                gameContainer.append(square);
+            }
         }
     };
 
